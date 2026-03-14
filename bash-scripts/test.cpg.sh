f52981fcf634c916a3576fac1219a7f86dc58243
manh-td <dmtran120901@gmail.com>
2026-03-14 17:33:11 +1100

    Update code

--- bash-scripts/test.cpg.sh
+++ bash-scripts/test.cpg.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 joern-parse "examples/" --output "joern-output/sample.cpg.bin"
-# joern-export --repr "cpg" --format dot --out "joern-output/cpg" "joern-output/sample.cpg.bin"
-joern --script "scripts/graph-for-funcs.sc" \
-	--param cpgFile="joern-output/sample.cpg.bin" \
-	--param outFile="joern-output/sample.cpg.json"
+joern --script "joern-scripts/graph-for-funcs.sc" --param cpgFile="joern-output/sample.cpg.bin" --param outFile="joern-output/sample.cpg.json"
